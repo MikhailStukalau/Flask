b10de17d45f59fa677d8c8df31398bcb4e8e5a5a
muxar <1>
2023-02-01 19:04:00 +0300

    update 3

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from logging.config import dictConfig
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import LOGGING
+import os
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 dictConfig(LOGGING)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.logger = logging.getLogger('app')
-app.logger.info("App started")
+# app.logger = logging.getLogger('app')
+
 db = SQLAlchemy(app)
 
 import views
+db.create_all()
+
 
--- views.py
+++ views.py
@@ -17,6 +17,12 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/post')
+def post():
+    articles = Article.query.order_by(Article.date).all()
+    return render_template('posts.html', articles=articles)
+
+
 @app.route('/create', methods=['POST', 'GET'])
 def create():
     if request.method == 'POST':
@@ -28,6 +34,7 @@ def create():
 
         try:
             db.session.add(art)
+            app.logger.info(f"Entry with title {title} added!")
             db.session.commit()
             return redirect('/home')
         except Exception as ex:
@@ -35,6 +42,22 @@ def create():
             return 'Error'
     else:
         return render_template('create.html')
+
+
+@app.route('/<int:id>/details/', methods=['GET'])
+def details(id):
+    detail = None
+    try:
+        detail = Article.query.get_or_404(id)
+        db.session.commit()
+        if detail is None:
+            flash('Нет записи с таким идентификатором', 'danger')
+            abort(404)
+    except SQLAlchemyError as ex:
+        log_error('Error while querying database', exc_info=ex)
+        flash('Во время запроса произошла непредвиденная ошибка', 'danger')
+        abort(500)
+    return render_template('details.html', detail=detail)
 
 
 @app.route('/contacts')
